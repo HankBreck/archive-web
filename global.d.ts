dfa957ecc24958fc44aa3940a5164c6a33bb4323
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-09-12 16:58:13 -0600

    Remove old mongoose stuff

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,5 @@
-import { Mongoose } from "mongoose"
 import { S3Client } from '@aws-sdk/client-s3'
 
 export declare global {
-  var mongoose: {
-    conn: Mongoose | undefined,
-    promise: Promise<Mongoose> | undefined, // Hacky solution, fix
-  }
   var s3Client: S3Client | undefined
 }
