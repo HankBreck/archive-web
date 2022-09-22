13e9f4c93ced9f894c9857c8e0e9165492bf9c73
Hank Breckenridge <breckenridgeh2@gmail.com>
2022-09-22 10:29:15 -0600

    Clean up

--- pages/create/success.tsx
+++ pages/create/success.tsx
@@ -1,25 +1,7 @@
-import { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 
-import { Document, Page, pdfjs } from "react-pdf";
-import { Window as KeplrWindow } from '@keplr-wallet/types'
-import { Coin, isDeliverTxSuccess, StdFee } from "@cosmjs/stargate";
-
-import { fillContract, fillContractCdaId } from "../../lib/utils/pdf";
-import { getIPFSClient } from "../../lib/utils/ipfs";
-import api from "../../lib/utils/api-client";
-import { fetchOrSetTempCDA } from "../../lib/utils/cookies";
-import { bytesToUtf8 } from "../../lib/utils/binary"
-
-import { Ownership } from "archive-client-ts/archive.cda/types"
-import { chainConfig } from "../../lib/chain/chain";
-
 import styles from "../../styles/Home.module.css";
-
-import { getArchiveClient } from '../../lib/utils/archive'
-import { MsgCreateCDA } from 'archive-client-ts/archive.cda/types/cda/tx'
-import { PostBody } from "../api/cda/contract";
 
 const SuccessPage: NextPage = () => {
     const router = useRouter()
